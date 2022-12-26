d08e073494a726753c925a51b19de9ab8a59f41d
shogo-nakano-desu <61229807+shogo-nakano-desu@users.noreply.github.com>
2022-12-26 13:07:29 +0000

    feat(backend): impl usecase

---
+++ apps/backend/src/core/usecase/user-answer/user-answer.usecase.ts
@@ -0,0 +1,24 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { UserAnswerInputParams } from 'src/api/user-answer/user-answer-input';
+import { USER_ANSWER_REPOSITORY } from '../../../core/constants';
+import { UserAnswer } from '../../../core/domain/user-answer/user-answer';
+import { UserAnswerRepository } from 'src/core/domain/user-answer/user-answer.repository';
+
+@Injectable()
+export class UserAnswerUsecase {
+  constructor(
+    @Inject(USER_ANSWER_REPOSITORY)
+    private readonly repository: UserAnswerRepository,
+  ) {}
+
+  async insertUserAnswer(params: UserAnswerInputParams): Promise<void> {
+    this.repository.save(
+      UserAnswer.reconstruct({
+        userId: params.userId,
+        sessionId: params.sessionId,
+        answer: params.answer,
+        requestedAt: params.requestedAt,
+      }),
+    );
+  }
+}
