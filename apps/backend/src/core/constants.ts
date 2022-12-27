38da4768dd4ef8706490804c5a8deb0b1e004215
shogo-nakano-desu <61229807+shogo-nakano-desu@users.noreply.github.com>
2022-12-27 06:48:26 +0000

    feat(backend): impl core

--- apps/backend/src/core/constants.ts
+++ apps/backend/src/core/constants.ts
@@ -1,2 +1,3 @@
 export const USER_ANSWER_REPOSITORY = 'UserAnswerRepository';
+export const SESSION_DETAIL_REPOSITORY = 'SessionDetailRepository';
 export const DATASOURCE_CLIENT = 'DatasourceClient';
