029f6fe431f5ff09f47ece4f04a3e378f225e7d9
shogo-nakano-desu <61229807+shogo-nakano-desu@users.noreply.github.com>
2022-12-28 06:42:22 +0000

    feat(backend): impl module

--- apps/backend/src/core/constants.ts
+++ apps/backend/src/core/constants.ts
@@ -1,3 +1,5 @@
+export const RESULT_SUMMARY_QUERY_SERVICE = 'ResultSummaryQueryService';
 export const USER_ANSWER_REPOSITORY = 'UserAnswerRepository';
 export const SESSION_DETAIL_REPOSITORY = 'SessionDetailRepository';
 export const DATASOURCE_CLIENT = 'DatasourceClient';
+export const QUERY_SERVICE_CLIENT = 'QueryServiceClient';
--- apps/backend/src/core/core.module.ts
+++ apps/backend/src/core/core.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { UserAnswerUsecase } from './usecase/user-answer/user-answer.usecase';
 import {
   DATASOURCE_CLIENT,
+  QUERY_SERVICE_CLIENT,
+  RESULT_SUMMARY_QUERY_SERVICE,
   SESSION_DETAIL_REPOSITORY,
   USER_ANSWER_REPOSITORY,
 } from './constants';
@@ -9,16 +11,33 @@ import { UserAnswerDatasource } from './infra/user-answer/user-answer.datasource
 import { DatasourceClient } from './infra/datasource.client';
 import { SessionDetailUsecase } from './usecase/session-detail/session-detail.usecase';
 import { SessionDetailDatasource } from './infra/session-detail/session-detail.datasource';
+import { ResultSummaryQueryDatasource } from './infra/result-summary/result-summary-query.datasource';
+import { QueryServiceClient } from './infra/query-service';
 
 @Module({
   imports: [],
   providers: [
     UserAnswerUsecase,
     SessionDetailUsecase,
+    {
+      provide: RESULT_SUMMARY_QUERY_SERVICE,
+      useClass: ResultSummaryQueryDatasource,
+    },
     { provide: USER_ANSWER_REPOSITORY, useClass: UserAnswerDatasource },
     { provide: SESSION_DETAIL_REPOSITORY, useClass: SessionDetailDatasource },
     { provide: DATASOURCE_CLIENT, useValue: DatasourceClient.getInstance() },
+    {
+      provide: QUERY_SERVICE_CLIENT,
+      useValue: QueryServiceClient.getInstance(),
+    },
   ],
-  exports: [UserAnswerUsecase, SessionDetailUsecase],
+  exports: [
+    UserAnswerUsecase,
+    SessionDetailUsecase,
+    {
+      provide: RESULT_SUMMARY_QUERY_SERVICE,
+      useClass: ResultSummaryQueryDatasource,
+    },
+  ],
 })
 export class CoreModule {}
