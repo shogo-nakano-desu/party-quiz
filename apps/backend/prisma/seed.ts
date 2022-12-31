28c4c710883eb3b21a53f16d377ce6cff026f8e0
shogo-nakano-desu <61229807+shogo-nakano-desu@users.noreply.github.com>
2022-12-31 05:27:45 +0000

    test(backend): add seed data

--- apps/backend/prisma/seed.ts
+++ apps/backend/prisma/seed.ts
@@ -1,52 +1,145 @@
-// TODO will be implemented
-// import { PrismaClient } from '@prisma/client';
-// const prisma = new PrismaClient();
-// async function main() {
-//   const alice = await prisma.user.upsert({
-//     where: { email: 'alice@prisma.io' },
-//     update: {},
-//     create: {
-//       email: 'alice@prisma.io',
-//       name: 'Alice',
-//       posts: {
-//         create: {
-//           title: 'Check out Prisma with Next.js',
-//           content: 'https://www.prisma.io/nextjs',
-//           published: true,
-//         },
-//       },
-//     },
-//   });
-//   const bob = await prisma.user.upsert({
-//     where: { email: 'bob@prisma.io' },
-//     update: {},
-//     create: {
-//       email: 'bob@prisma.io',
-//       name: 'Bob',
-//       posts: {
-//         create: [
-//           {
-//             title: 'Follow Prisma on Twitter',
-//             content: 'https://twitter.com/prisma',
-//             published: true,
-//           },
-//           {
-//             title: 'Follow Nexus on Twitter',
-//             content: 'https://twitter.com/nexusgql',
-//             published: true,
-//           },
-//         ],
-//       },
-//     },
-//   });
-//   console.log({ alice, bob });
-// }
-// main()
-//   .then(async () => {
-//     await prisma.$disconnect();
-//   })
-//   .catch(async (e) => {
-//     console.error(e);
-//     await prisma.$disconnect();
-//     process.exit(1);
-//   });
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+async function main() {
+  const sessionIds: [string, string] = ['sesn-1', 'sesn-2'];
+  const questionIds = ['qstn-1', 'qstn-2', 'qstn-3', 'qstn-4', 'qstn-5'];
+  const sessionDetailIds = ['sesd-1', 'sesd-2', 'sesd-3', 'sesd-4', 'sesd-5'];
+
+  await createQuestionSessionSessionDetail(
+    prisma,
+    sessionIds,
+    questionIds,
+    sessionDetailIds,
+  );
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
+
+async function createQuestionSessionSessionDetail(
+  client: PrismaClient,
+  sessionIds: [string, string],
+  questionIds: string[],
+  sessionDetailIds: string[],
+): Promise<void> {
+  const now = new Date();
+  await client.question.createMany({
+    data: [
+      {
+        id: questionIds[0],
+        name: "What is Shogo's favorite food?",
+        option_1: 'Strawberry',
+        option_2: 'Chips',
+        option_3: 'Grapes',
+        option_4: 'Chocolate',
+        answer: 'option_3',
+        created_at: now,
+      },
+      {
+        id: questionIds[1],
+        name: 'How old is Shogo?',
+        option_1: '19',
+        option_2: '20',
+        option_3: '27',
+        option_4: '28',
+        answer: 'option_3',
+        created_at: now,
+      },
+      {
+        id: questionIds[2],
+        name: 'Where is the capital in Japan?',
+        option_1: 'Tokyo',
+        option_2: 'Kyoto',
+        option_3: 'Osaka',
+        option_4: 'Kobe',
+        answer: 'option_1',
+        created_at: now,
+      },
+      {
+        id: questionIds[3],
+        name: 'Which programming language does shogo use?',
+        option_1: 'OCaml',
+        option_2: 'Rust',
+        option_3: 'Closure',
+        option_4: 'Kotlin',
+        answer: 'option_2',
+        created_at: now,
+      },
+      {
+        id: questionIds[4],
+        name: "When is Shogo's birthday?",
+        option_1: '24th Dec',
+        option_2: '18th Aug',
+        option_3: '15th Jan',
+        option_4: '16th May',
+        answer: 'option_4',
+        created_at: now,
+      },
+    ],
+  });
+  await client.session.createMany({
+    data: [
+      {
+        id: sessionIds[0],
+        name: 'wedding-party',
+        created_at: now,
+      },
+      {
+        id: sessionIds[1],
+        name: 'graduation-party',
+        created_at: now,
+      },
+    ],
+  });
+  await client.session_detail.createMany({
+    data: [
+      {
+        id: sessionDetailIds[0],
+        number: 1,
+        session_id: sessionIds[0],
+        question_id: questionIds[0],
+        started_at: new Date('2022-12-27 15:10:00'),
+        ended_at: new Date('2022-12-27 15:10:30'),
+      },
+      {
+        id: sessionDetailIds[1],
+        number: 2,
+        session_id: sessionIds[0],
+        question_id: questionIds[1],
+        started_at: new Date('2022-12-27 15:10:31'),
+        ended_at: new Date('2022-12-27 15:11:01'),
+      },
+      {
+        id: sessionDetailIds[2],
+        number: 3,
+        session_id: sessionIds[0],
+        question_id: questionIds[2],
+        started_at: new Date('2022-12-27 15:11:02'),
+        ended_at: new Date('2022-12-27 15:11:32'),
+      },
+      {
+        id: sessionDetailIds[3],
+        number: 4,
+        session_id: sessionIds[1],
+        question_id: questionIds[3],
+        started_at: new Date('2022-12-27 15:11:33'),
+        ended_at: new Date('2022-12-27 15:12:03'),
+      },
+      {
+        id: sessionDetailIds[4],
+        number: 5,
+        session_id: sessionIds[1],
+        question_id: questionIds[4],
+        started_at: new Date('2022-12-27 15:12:04'),
+        ended_at: new Date('2022-12-27 15:12:34'),
+      },
+    ],
+  });
+}
