3d71659e87d0421e04cd5f9ac9899580c72936fc
shogo-nakano-desu <61229807+shogo-nakano-desu@users.noreply.github.com>
2023-01-14 13:15:47 +0000

    feat(web): style user page

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import { UrqlProvider } from '../../graphql/urqlProvider';
+import { ChakraProvider } from '@chakra-ui/react';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <UrqlProvider>
-      <Component {...pageProps} />
-    </UrqlProvider>
+    <ChakraProvider>
+      <UrqlProvider>
+        <Component {...pageProps} />
+      </UrqlProvider>
+    </ChakraProvider>
   );
 }
