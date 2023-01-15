5d0d2de6756b52f65140165609e96155a896bbb0
shogo-nakano-desu <61229807+shogo-nakano-desu@users.noreply.github.com>
2023-01-15 15:42:46 +0000

    fix(ui): fix css

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -2,10 +2,20 @@ import { UrqlProvider } from '../../graphql/urqlProvider';
 import { ChakraProvider } from '@chakra-ui/react';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { extendTheme } from '@chakra-ui/react';
+// 2. custom colorやfontなどで theme を拡張する
+const colors = {
+  brand: {
+    900: '#1a365d',
+    800: '#153e75',
+    700: '#2a69ac',
+  },
+}
+const theme = extendTheme({ colors })
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <UrqlProvider>
         <Component {...pageProps} />
       </UrqlProvider>
