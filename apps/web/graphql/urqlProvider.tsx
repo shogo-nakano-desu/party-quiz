7be66fb2b2402855d33f89c0a270a68a5acb9f36
shogo-nakano-desu <61229807+shogo-nakano-desu@users.noreply.github.com>
2023-01-19 23:50:38 +0000

    refactor(backend): delete unnecesary log

--- apps/web/graphql/urqlProvider.tsx
+++ apps/web/graphql/urqlProvider.tsx
@@ -2,8 +2,6 @@ import { PropsWithChildren } from 'react';
 import { createClient, Provider } from 'urql';
 
 const baseUrl = (process.env.NODE_ENV === 'test'|| process.env.NODE_ENV ==='development' )? process.env.NEXT_PUBLIC_API_ENDPOINT_DEV : process.env.NEXT_PUBLIC_API_ENDPOINT_PROD;
-console.log(baseUrl)
-console.log(process.env.NODE_ENV)
 // https://formidable.com/open-source/urql/docs/basics/react-preact/#providing-the-client
 const client = createClient({
   url: `${baseUrl}/graphql`,
