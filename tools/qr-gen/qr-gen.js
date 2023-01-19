417bfd28809dcdc019dfc71e72eaaffed16ca5ce
shogo-nakano-desu <61229807+shogo-nakano-desu@users.noreply.github.com>
2023-01-19 23:51:03 +0000

    fix(qr-gen): fix url schema

--- tools/qr-gen/qr-gen.js
+++ tools/qr-gen/qr-gen.js
@@ -6,14 +6,14 @@ const urls = [];
 const fileNames = [];
 
 // ./list.csv is supposed to have only one column that contains URL.
-// The URL is supposed to be like 'https://quiz.dev/party?name=john-doe'
+// The URL is supposed to be 'https://shogo-moe-wedding.vercel.app/menu/firstname-lastname'
+// We need to put column name at the top row of the csv.
 fs.createReadStream('./list.csv')
   .pipe(csvParser())
   .on('data', (data) => {
     const url = Object.values(data)[0];
     urls.push(url);
-    let cutFromIndex = url.indexOf('=')+1;
-    fileNames.push(url.substring(cutFromIndex)); 
+    fileNames.push(url.substring(42)); 
   }).on('end', () => {
     urls.forEach((url, i) => QRCode.toFile(`./generated/${fileNames[i]}.png`, url))
   })
